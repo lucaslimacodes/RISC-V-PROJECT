8ab975f368f9e8d02244bdbbf32ea2fbc645e7ec
Lucas de Holanda Lima <lhl@WINDOWS.CIN.UFPE.BR>
2023-08-17 11:57:04 -0300

    feito os bagulho ai

--- design/ALUController.sv
+++ design/ALUController.sv
@@ -12,18 +12,22 @@ module ALUController (
 
   assign Operation[0] = ((ALUOp == 2'b10) && (Funct3 == 3'b110)) ||  // R\I-or
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0000000)) ||  // R\I->>
-      ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000));  // R\I->>>
-
+      ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) ||  // R\I->>>
+      ((ALUOp == 2'b10) && (Funct3 == 3'b100) && (Funct7 == 7'b0000000)) || // xor
+      ((ALUOp == 2'b01) && (Funct3 == 3'b001)) || // BNE
+      ((ALUOp == 2'b01) && (Funct3 == 3'b101)); //BGE
   assign Operation[1] = (ALUOp == 2'b00) ||  // LW\SW
       ((ALUOp == 2'b10) && (Funct3 == 3'b000)) ||  // R\I-add, SUB
-      ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000));  // R\I->>>
-
+      ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) ||  // R\I->>>
+      ((ALUOp == 2'b10) && (Funct3 == 3'b100) && (Funct7 == 7'b0000000)) || //xor
+      ((ALUOp == 2'b01) && (Funct3 == 3'b100)) || // BLT 
+      ((ALUOp == 2'b01) && (Funct3 == 3'b101)); //BGE
   assign Operation[2] =  ((ALUOp==2'b10) && (Funct3==3'b101) && (Funct7==7'b0000000)) || // R\I->>
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) ||  // R\I->>>
       ((ALUOp == 2'b10) && (Funct3 == 3'b001)) ||  // R\I-<<
       ((ALUOp == 2'b10) && (Funct3 == 3'b010)) ||  // R\I-<
       ((ALUOp == 2'b10) && (Funct3 == 3'b000) && (Funct7 == 7'b0100000)); //SUB
 
-  assign Operation[3] = (ALUOp == 2'b01) ||  // BEQ
+  assign Operation[3] = (ALUOp == 2'b01) ||  // BEQ, BNE, BLT, BGE
       ((ALUOp == 2'b10) && (Funct3 == 3'b010));  // R\I-<
 endmodule
